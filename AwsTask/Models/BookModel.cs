ce3f3bd599e5c3e3f700ea7a1253a9a929bc118e
KEP11 <kolesnikov.e@gmail.com>
2022-12-20 18:58:52 +0600

    Update BookModel.cs

--- AwsTask/Models/BookModel.cs
+++ AwsTask/Models/BookModel.cs
@@ -1,7 +1,10 @@
+using Amazon.DynamoDBv2.DataModel;
+
 namespace AwsTask.Models;
 
 public class BookModel
 {
+    [DynamoDBHashKey]
     public string Isbn { get; set; }
     
     public string? Title { get; set; }
